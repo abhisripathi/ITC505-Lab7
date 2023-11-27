4e2f10ebad16df85b575b64d8e024e2b9fe6cbc2
Purnabhishek Sripathi <76512306+abhisripathi@users.noreply.github.com>
2023-11-27 16:26:08 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+// server.js or index.js (at the project root or inside an `/api` directory)
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
+  
+app.get('/cs212/homework/8', (req, res) => {
+    res.sendFile('/index.html', { root: '.' }); // The root option specifies the root directory for the sendFile method.
+});
+
+app.post('/cs212/homework/8', (req, res) => {
+    const { adjective, pluralNoun, personName, verb, noun } = req.body;
+    const madLib = `Today, I saw a ${adjective} ${pluralNoun} which made me think of ${personName}. 
+                    Then I decided to ${verb} by the ${noun}.`;
+
+    res.send(madLib);
+});
+
+module.exports = app;
