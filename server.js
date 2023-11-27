70aa1f9683802c4304ddb277e917ce6890eca6f7
Purnabhishek Sripathi <76512306+abhisripathi@users.noreply.github.com>
2023-11-27 16:39:44 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,14 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-  });
+// Add a route for the root URL
+app.get('/', (req, res) => {
+    res.send('Welcome to my Node.js project!'); // You can adjust this to serve an HTML page if needed
+});
+
+// app.listen(3000, () => {
+//     console.log('Server is running on port 3000');
+//   });
   
 app.get('/cs212/homework/8', (req, res) => {
     res.sendFile('/index.html', { root: '.' }); // The root option specifies the root directory for the sendFile method.
